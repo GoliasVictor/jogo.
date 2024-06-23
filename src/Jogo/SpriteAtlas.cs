9ef478cf947e4207437b2d04374c42c619f53996
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-23 14:07:21 -0300

    Throw exception on SpriteAtlas.GetSprite

--- src/Jogo/SpriteAtlas.cs
+++ src/Jogo/SpriteAtlas.cs
@@ -44,11 +44,24 @@ public class SpriteAtlas {
     /// </summary>
     /// <param name="i">Row of the desired sprite.</param>
     /// <param name="j">Column of the desired sprite.</param>
-    /// <returns>In case the position is valid, returns the sprite in the desired position, otherwise returns the sprite in the nearest valid position.</returns>
+    /// <returns>In case the position is valid, returns the sprite in the desired position.</returns>
+    /// <exception cref="IndexOutOfRangeException">Invalid desired position.</exception>
     public static Texture2D GetSprite(int i, int j) {
-        if(instance._rows == 0 || instance._columns == 0) return new();
-        i = Math.Clamp(i, 0, (int)instance._rows - 1);
-        j = Math.Clamp(j, 0, (int)instance._columns - 1);
-        return instance._sprites[i, j];        
+        try {
+            return instance._sprites[i, j];        
+        } catch(IndexOutOfRangeException e) {
+            bool iValid = (i >= 0) && (i < instance._rows);
+            bool jValid = (j >= 0) && (j < instance._columns);
+            string exceptionDescription;
+            if(!iValid && !jValid) {
+                exceptionDescription = $"<Row {i}; Column {j}> not available in Atlas with {instance._rows}x{instance._columns}";
+            }else if(!iValid) {
+                exceptionDescription = $"Row {i} not avaliable in Atlas with {instance._rows} rows.";
+            }else {
+                exceptionDescription = $"Column {i} not avaliable in Atlas with {instance._columns} Columns.";
+            }
+
+            throw new IndexOutOfRangeException(exceptionDescription, e);
+        }
     }
 }
