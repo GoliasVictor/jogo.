5f71c17c38c88dc322456a19f60d5c73cfee3d72
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-24 13:40:08 -0300

    Add token sintax to LevelLoader

--- src/Jogo/LevelLoader.cs
+++ src/Jogo/LevelLoader.cs
@@ -5,19 +5,19 @@ using YamlDotNet.Serialization.NamingConventions;
 /// Contains predefined token chars for level building.
 /// </summary>
 static class LevelToken {
-    const char Player = 'p';
-    const char Wall = 'w';
-    const char Goal = 'g';
-    const char Enemy = 'e';
-    const char Water = '1';
-    const char Grass = '2';
-    const char Fire = '3';
-    const char Horizontal = 'h';
-    const char Vertical = 'v';
-    const char Item = 'i';
-    const char Key = 'k';
-    const char Door = 'd';
-    const char None = '_';
+    public const char Player = 'p';
+    public const char Wall = 'w';
+    public const char Goal = 'g';
+    public const char Enemy = 'e';
+    public const char Water = '1';
+    public const char Grass = '2';
+    public const char Fire = '3';
+    public const char Horizontal = 'h';
+    public const char Vertical = 'v';
+    public const char Item = 'i';
+    public const char Key = 'k';
+    public const char Door = 'd';
+    public const char None = '_';
 
 }
 
@@ -61,7 +61,7 @@ static class LevelLoader
         int rows = map_rows.Length;
         int columns = 0;
 
-        string [][] token_map = new string[rows][];
+        string [][] tokenMap = new string[rows][];
         
         foreach (string line in map_rows) {
             string [] current_row = line.Split(' ');
@@ -70,24 +70,35 @@ static class LevelLoader
             if (length > columns) {
                 columns = length;
             }
-            token_map[Array.IndexOf(map_rows,line)] = current_row;
+            tokenMap[Array.IndexOf(map_rows,line)] = current_row;
         }
 
         List<IEntity> map_entities = [];
 
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < token_map[i].Length; j++) {
-                map_entities.Add(token_map[i][j] switch {
-                    "b" => new Box(new GridVec2(i,j)),
-                    
-                    "d" => new DoorEntity(new GridVec2(i,j)),
-
-                    "f" => new FireEntity(new GridVec2(i,j)),
-
-                    "o" => new Wall(new GridVec2(i,j)),
-
-                    "w" => new WaterEntity(new GridVec2(i,j)),
-                });
+            for (int j = 0; j < tokenMap[i].Length; j++) {
+                GridVec2 position = new GridVec2(i, j);
+                IEntity? entity = tokenMap[i][j][0] switch {
+                    LevelToken.Player => new Player(position),
+                    LevelToken.Door => new DoorEntity(position),
+                    LevelToken.Enemy => tokenMap[i][j][1] switch {
+                        LevelToken.Water => new Enemy(position, Element.Water, tokenMap[i][j][2] == LevelToken.Horizontal),
+                        LevelToken.Grass => new Enemy(position, Element.Water, tokenMap[i][j][2] == LevelToken.Horizontal),
+                        LevelToken.Fire => new Enemy(position, Element.Water, tokenMap[i][j][2] == LevelToken.Horizontal),
+                        _ => null
+                    },
+                    LevelToken.Item => tokenMap[i][j][1] switch {
+                        LevelToken.Water => new ElementChangerEntity(position, Element.Water),
+                        LevelToken.Grass => new ElementChangerEntity(position, Element.Leaf),
+                        LevelToken.Fire => new ElementChangerEntity(position, Element.Fire),
+                        _ => null
+                    },
+                    LevelToken.Water => new WaterEntity(position),
+                    LevelToken.Grass => new Box(position),
+                    LevelToken.Fire => new FireEntity(position),
+                    _ => null
+                };
+                if(entity != null) map_entities.Add(entity);
             }
         }
         return new Map(rows, columns, map_entities);
